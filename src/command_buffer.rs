4dbbf949b3cb286e1dc763090420bb6bb249a623
cynic64 <f3bl@gmx.com>
2019-07-23 17:10:43 +0200

    formatting

--- src/command_buffer.rs
+++ src/command_buffer.rs
@@ -1,13 +1,15 @@
 extern crate vulkano;
-use vulkano::command_buffer::{AutoCommandBuffer, AutoCommandBufferBuilder, DynamicState, CommandBufferExecFuture};
+use vulkano::buffer::BufferAccess;
+use vulkano::command_buffer::{
+    AutoCommandBuffer, AutoCommandBufferBuilder, CommandBufferExecFuture, DynamicState,
+};
+use vulkano::descriptor::DescriptorSet;
 use vulkano::device::{Device, Queue};
+use vulkano::format::ClearValue;
 use vulkano::framebuffer::FramebufferAbstract;
 use vulkano::pipeline::GraphicsPipelineAbstract;
-use vulkano::descriptor::DescriptorSet;
-use vulkano::format::ClearValue;
-use vulkano::buffer::BufferAccess;
-use vulkano::sync::{FenceSignalFuture, GpuFuture, FlushError};
 use vulkano::swapchain::{PresentFuture, Swapchain};
+use vulkano::sync::{FenceSignalFuture, FlushError, GpuFuture};
 
 use std::sync::Arc;
 
@@ -19,56 +21,62 @@ pub struct ConcreteObject {
     pub uniform_set: Arc<DescriptorSet + Send + Sync>,
 }
 
-pub fn create_command_buffer(device: Arc<Device>, queue: Arc<Queue>, framebuffer: Arc<FramebufferAbstract + Send + Sync>, clear_values: &[ClearValue], objects: &[ConcreteObject]) -> AutoCommandBuffer {
-    let mut command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(
-        device.clone(),
-        queue.family(),
-    )
-    .unwrap()
-    .begin_render_pass(
-        framebuffer.clone(),
-        false,
-        clear_values.to_vec(),
-    )
-    .unwrap();
+pub fn create_command_buffer(
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    framebuffer: Arc<FramebufferAbstract + Send + Sync>,
+    clear_values: &[ClearValue],
+    objects: &[ConcreteObject],
+) -> AutoCommandBuffer {
+    let mut command_buffer =
+        AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
+            .unwrap()
+            .begin_render_pass(framebuffer.clone(), false, clear_values.to_vec())
+            .unwrap();
 
     for object in objects.iter() {
-        command_buffer = command_buffer.draw(
-            object.pipeline.clone(),
-            &object.dynamic_state,
-            vec![object.vertex_buffer.clone()],
-            object.uniform_set.clone(),
-            (),
-        )
-        .unwrap();
+        command_buffer = command_buffer
+            .draw(
+                object.pipeline.clone(),
+                &object.dynamic_state,
+                vec![object.vertex_buffer.clone()],
+                object.uniform_set.clone(),
+                (),
+            )
+            .unwrap();
     }
 
-    command_buffer
-        .end_render_pass()
-        .unwrap()
-        .build()
-        .unwrap()
+    command_buffer.end_render_pass().unwrap().build().unwrap()
 }
 
-pub fn submit_command_buffer_to_swapchain<W, F>(device: Arc<Device>, queue: Arc<Queue>, future: F, swapchain: Arc<Swapchain<W>>, image_num: usize, command_buffer: AutoCommandBuffer) -> Result<FenceSignalFuture<PresentFuture<CommandBufferExecFuture<F, AutoCommandBuffer>, W>>, FlushError>
-    where F: GpuFuture + 'static,
+pub fn submit_command_buffer_to_swapchain<W, F>(
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    future: F,
+    swapchain: Arc<Swapchain<W>>,
+    image_num: usize,
+    command_buffer: AutoCommandBuffer,
+) -> Result<
+    FenceSignalFuture<PresentFuture<CommandBufferExecFuture<F, AutoCommandBuffer>, W>>,
+    FlushError,
+>
+where
+    F: GpuFuture + 'static,
 {
     submit_command_buffer_partial(queue.clone(), future, command_buffer)
-        .then_swapchain_present(
-            queue,
-            swapchain,
-            image_num,
-        )
+        .then_swapchain_present(queue, swapchain, image_num)
         .then_signal_fence_and_flush()
 }
 
-fn submit_command_buffer_partial<F>(queue: Arc<Queue>, future: F, command_buffer: AutoCommandBuffer) -> CommandBufferExecFuture<F, AutoCommandBuffer>
-    where F: GpuFuture + 'static,
+// pub fn check_if_swapchain_outdated
+
+fn submit_command_buffer_partial<F>(
+    queue: Arc<Queue>,
+    future: F,
+    command_buffer: AutoCommandBuffer,
+) -> CommandBufferExecFuture<F, AutoCommandBuffer>
+where
+    F: GpuFuture + 'static,
 {
-    future
-        .then_execute(
-            queue.clone(),
-            command_buffer,
-        )
-        .unwrap()
+    future.then_execute(queue.clone(), command_buffer).unwrap()
 }
